4150d6954d2caf45159c8cfbc07914c79a2dd24d
pranishrajbhandari <rajbhandari.pranish@gmail.com>
2026-05-15 07:58:23 +0545

    updated cors and refresh token config

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,14 @@ const app = express();
 
 setupSwagger(app);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FE_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
